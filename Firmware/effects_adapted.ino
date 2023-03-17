18066e102580b5a8f30ff2a91c4d684eb1896241
SlingMaster <alexdovby@gmail.com>
2023-03-17 13:54:42 +0200

    added effect Mirage

--- Firmware/effects_adapted.ino
+++ Firmware/effects_adapted.ino
@@ -408,3 +408,87 @@ void EffectStars() {
   }
   blur2d(leds, WIDTH, HEIGHT, blur);
 }
+
+
+// =====================================
+//                Mirage
+//               © Stepko
+//                Міраж
+// =====================================
+byte buff[WIDTH + 2][HEIGHT + 2];
+// -------------------------------------
+void blur() {
+  uint16_t sum;
+  for (byte x = 1; x < WIDTH + 1; x++) {
+    for (byte y = 1; y < HEIGHT + 1; y++) {
+      sum = buff[x][y];
+      sum += buff[x + 1][y];
+      sum += buff[x][y - 1];
+      sum += buff[x][y + 1];
+      sum += buff[x - 1][y];
+      sum /= 5;
+      buff[x][y] = sum;
+    }
+  }
+}
+
+// -------------------------------------
+void drawDot(float x, float y, byte a) {
+  uint8_t xx = (x - (int) x) * 255, yy = (y - (int) y) * 255, ix = 255 - xx, iy = 255 - yy;
+#define WU_WEIGHT(a, b)((uint8_t)(((a) * (b) + (a) + (b)) >> 8))
+  uint8_t wu[4] = {
+    WU_WEIGHT(ix, iy),
+    WU_WEIGHT(xx, iy),
+    WU_WEIGHT(ix, yy),
+    WU_WEIGHT(xx, yy)
+  };
+  // multiply the intensities by the colour, and saturating-add them to the pixels
+  for (uint8_t i = 0; i < 4; i++) {
+    int16_t xn = x + (i & 1), yn = y + ((i >> 1) & 1);
+    byte clr = buff[xn][yn];
+    clr = constrain(qadd8(clr, (a * wu[i]) >> 8), 0 , 255);
+    buff[xn][yn] = clr;
+  }
+}
+
+// -------------------------------------
+void Mirage() {
+  const uint8_t divider = 4;
+  const uint8_t val = 255;
+  if (loadingFlag) {
+#if defined(USE_RANDOM_SETS_IN_APP) || defined(RANDOM_SETTINGS_IN_CYCLE_MODE)
+    if (selectedSettings) {
+      //                     scale | speed
+      setModeSettings(random8(100U), random8(80U, 255U));
+    }
+#endif //#if defined(USE_RANDOM_SETS_IN_APP) || defined(RANDOM_SETTINGS_IN_CYCLE_MODE)
+    loadingFlag = false;
+    hue = 70;
+  }
+
+  blur();
+  float x1 = (float)beatsin88(15 * modes[currentMode].Speed, divider, WIDTH * divider) / divider;
+  float y1 = (float)beatsin88(20 * modes[currentMode].Speed, divider, HEIGHT * divider) / divider;
+  float x2 = (float)beatsin88(16 * modes[currentMode].Speed, divider, (WIDTH - 1) * divider) / divider;
+  float y2 = (float)beatsin88(14 * modes[currentMode].Speed, divider, HEIGHT * divider) / divider;
+  float x3 = (float)beatsin88(12 * modes[currentMode].Speed, divider, (WIDTH - 1) * divider) / divider;
+  float y3 = (float)beatsin88(16 * modes[currentMode].Speed, divider, HEIGHT * divider) / divider;
+
+  drawDot(x1 , y1, val);
+  drawDot(x1 + 1, y1, val);
+  drawDot(x2 , y2, val);
+  drawDot(x2 + 1, y2, val);
+  drawDot(x3 , y3, val);
+  drawDot(x3 + 1, y3, val);
+
+  hue++;
+  for (byte x = 1; x < WIDTH + 1; x++) {
+    for (byte y = 1; y < HEIGHT + 1; y++) {
+      leds[XY(x - 1, y - 1)] = CHSV(hue , buff[x][y], 255);
+    }
+  }
+}
+
+// ==============
+// END ==============
+// ==============
